a5c7981c08cfea9eb581906b07b93a39419f46cb
fajarot <fajarrahmadyanto@gmail.com>
2022-12-15 09:42:42 +0700

    Big update add DB, add DB method, and dll

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,25 @@
 import Express from "express";
 
-import { 
-    getDotaInfo, 
-    getHome 
+import {
+  getDotaInfo,
+  getHome,
+  tambahData,
+  showData,
 } from "../controllers/dotaController.js";
+
+import { tesOpenAI } from "../controllers/openai.js";
 
 const router = Express.Router();
 
+router.get("/", getHome);
+router.get("/home", getHome);
 
-router.get('/', getHome);
+router.get("/dota/:channel_name", getDotaInfo);
 
-router.get('/dota/:channel_id', getDotaInfo);
+router.get("/add/:data_name", tambahData);
 
+router.get("/show/:data_name", showData);
+
+router.get("/openai", tesOpenAI);
 
 export default router;
